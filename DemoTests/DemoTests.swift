5bd03ca0d50c204ec1990d49518bddd4028ca688
Akshay Pimprikar <akshaypimprikar@users.noreply.github.com>
2026-05-05 15:32:59 -0400

    Initial Commit

---
+++ DemoTests/DemoTests.swift
@@ -0,0 +1,19 @@
+//
+//  DemoTests.swift
+//  DemoTests
+//
+//  Created by Akshay Pimprikar on 5/5/26.
+//
+
+import Testing
+@testable import Demo
+
+struct DemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
